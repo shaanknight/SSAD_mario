20c46c079c5e5f6dee949ca3f7e72a0ee3105302
shaanknight <sayantanjana49@gmail.com>
2018-08-22 04:12:47 +0530

    background and scenes added

---
+++ background.py
@@ -0,0 +1,44 @@
+import random
+
+class Tree():
+
+    def __init__(self):
+        self.tre = [
+            [' ',' ','/','\\',' ',' '],
+            [' ','/',' ',' ','\\',' '],
+            ['/','_',' ',' ','_','\\'],
+            [' ','/',' ',' ','\\',' '],
+            ['/','_','_','_','_','\\'],
+            ['_','_','|','|','_','_']
+        ]
+
+        self.pos_x = 12
+        self.pos_y = random.randint(10, 380)
+
+    def draw(self, box, org):
+        for i in range(len(self.tre)):
+            for j in range(len(self.tre[i])):
+                box[self.pos_x + i][org[0] + self.pos_y + j] = self.tre[i][j]
+
+    def __str__(self):
+        return str(self.pos_x) + ',' + str(self.pos_y)
+
+class Cloud():
+
+    def __init__(self):
+        self.str = [
+            [' ', ' ', ' ', '_', '_', ' ', ' ', ' '],
+            [' ', '_', '(', ' ', ' ', ')', '_', ' '],
+            ['(', '_', '_', '_', '_', '_', '_', ')']
+        ]
+
+        self.pos_x = random.randint(5, 10)
+        self.pos_y = random.randint(0, 360)
+    
+    def draw(self, box, org):
+        for i in range(len(self.str)):
+            for j in range(len(self.str[i])):
+                box[self.pos_x + i][org[0] + self.pos_y + j] = self.str[i][j]
+    
+    def __str__(self):
+        return str(self.pos_x) + ',' + str(self.pos_y)
--- main.py
+++ main.py
@@ -2,14 +2,43 @@ import os
 
 from board import Board
 from person import ppl,Mario
+from background import Tree,Cloud
 
 board = Board()
 mario = Mario()
 org = [0]
+arr_trp = []
+arr_cld = []
 
 while True :
 	os.system("tput reset")
 	mario.draw(board.box,org)
+
+	trp = Tree()
+
+	if arr_trp != []:
+		for i in arr_trp:
+			if abs(trp.pos_y - i.pos_y) > 10:
+				if len(arr_trp) <= 20:
+					trp.draw(board.box, org)
+					arr_trp.append(trp)
+
+	else:
+		arr_trp.append(trp)
+		trp.draw(board.box,org)
+
+	cld = Cloud()
+
+	if arr_cld != []:
+		for i in arr_cld:
+			if abs(cld.pos_y - i.pos_y) > 10:
+				if len(arr_cld) <= 20:
+					cld.draw(board.box, org)
+					arr_cld.append(cld)
+
+	else:
+		arr_cld.append(cld)
+		cld.draw(board.box, org)
 
 	board.draw(org)
 	mario.move(board,board.box,org)
